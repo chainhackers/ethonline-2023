6f4fbd1419c61cea903ca8735f1f625929951f01
etsvigun <utgarda@gmail.com>
2023-10-08 22:27:32 +0300

    forge install openzeppelin=OpenZeppelin/openzeppelin-contracts@v5.0.0 && forge install safe-global=safe-global/safe-core-protocol #5

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,7 +1,8 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {ISafeProtocolPlugin} from "@safe-global/interfaces/Modules.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
