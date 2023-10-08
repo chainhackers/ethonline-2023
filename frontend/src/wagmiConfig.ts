cd2bfcb2065013db6c487fe4f9580ba9dae30d0b
Joe Prime <101joe.prime@gmail.com>
2023-10-08 22:56:09 +0300

    hardcode projectId #4

--- frontend/src/wagmiConfig.ts
+++ frontend/src/wagmiConfig.ts
@@ -8,7 +8,7 @@ import {config} from "dotenv";
 
 config();
 
-const PROJECT_ID = process.env.PROJECT_ID!;
+const PROJECT_ID = 'f5f4ef3634a6aa0af5a1d5516608377a';
 
 export const { chains, publicClient } = configureChains(
   [mainnet, polygon, optimism, arbitrum, base, zora],
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import {defineConfig} from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import dotenvPlugin from 'vite-plugin-env-compatible'
+// import dotenvPlugin from 'vite-plugin-env-compatible'
 import {nodePolyfills} from 'vite-plugin-node-polyfills'
 
+//todo: fix dotenvPlugin
 export default defineConfig({
-    plugins: [reactRefresh(), dotenvPlugin(), nodePolyfills()],
+    plugins: [reactRefresh(), nodePolyfills()],
     define: {
         'process.env': process.env,
     },
