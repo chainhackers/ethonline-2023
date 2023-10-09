b8f5361a94e9681ffa455f50e78f33510b23c657
Joe Prime <101joe.prime@gmail.com>
2023-10-09 17:26:15 +0300

    check #4

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import {WagmiConfig} from "wagmi";
 import {chains, wagmiConfig} from './wagmiConfig.ts';
 import {RainbowKitProvider} from "@rainbow-me/rainbowkit";
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <WagmiConfig config={wagmiConfig}>
