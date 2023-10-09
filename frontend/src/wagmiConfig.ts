1b29205c74aae343438e697a1ee606fc6f9cc5f0
nadyasav <nadyasav28@gmail.com>
2023-10-09 14:06:52 +0300

    fix: wagmiConfig and .env file #4

--- frontend/src/wagmiConfig.ts
+++ frontend/src/wagmiConfig.ts
@@ -3,12 +3,9 @@ import {configureChains, createConfig, mainnet} from "wagmi";
 import {polygon} from "@wagmi/core/chains";
 import {getDefaultWallets} from "@rainbow-me/rainbowkit";
 import { arbitrum, base, optimism, zora } from 'viem/chains';
-import {config} from "dotenv";
 
-
-config();
-
-const PROJECT_ID = 'f5f4ef3634a6aa0af5a1d5516608377a';
+const PROJECT_ID = import.meta.env.VITE_PROJECT_ID!;
+console.log('walletConnect PROJECT_ID: ', PROJECT_ID);
 
 export const { chains, publicClient } = configureChains(
   [mainnet, polygon, optimism, arbitrum, base, zora],
