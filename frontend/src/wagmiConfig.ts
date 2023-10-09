fc7277b6e35f0d9104d72fa26d602374ad90a9b1
Joe Prime <101joe.prime@gmail.com>
2023-10-09 17:36:30 +0300

    hardcode project id #4

--- frontend/src/wagmiConfig.ts
+++ frontend/src/wagmiConfig.ts
@@ -4,7 +4,8 @@ import {polygon} from "@wagmi/core/chains";
 import {getDefaultWallets} from "@rainbow-me/rainbowkit";
 import { arbitrum, base, optimism, zora } from 'viem/chains';
 
-const PROJECT_ID = import.meta.env.VITE_PROJECT_ID!;
+// const PROJECT_ID = import.meta.env.VITE_PROJECT_ID!;
+const PROJECT_ID = "f5f4ef3634a6aa0af5a1d5516608377a";
 console.log('walletConnect PROJECT_ID: ', PROJECT_ID);
 
 export const { chains, publicClient } = configureChains(
