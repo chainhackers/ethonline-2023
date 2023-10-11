8cd1bfdf7332a3175666022db8e510345b4405d6
Joe Prime <101joe.prime@gmail.com>
2023-10-11 17:11:45 +0300

    add default paths to configs #9

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     target: 'es2020',
+  },
+  base: './',
+  resolve: {
+    alias: {
+      Components: path.resolve(__dirname, './src/components'),
+      Pages: path.resolve(__dirname, './src/pages'),
+    },
   },
   plugins: [react()],
 });
