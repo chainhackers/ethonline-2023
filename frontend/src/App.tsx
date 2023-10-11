33e965e4715b2535763105b5f9472b5a6f08507b
Joe Prime <101joe.prime@gmail.com>
2023-10-11 17:13:16 +0300

    add react router #9

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,57 +1,20 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './index.scss';
-import './App.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import DevModeToggle from './components/devModeToggle/DevModeToggle';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { PoolsPage } from 'Pages';
+import Root from 'Pages/root.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+  {
+    path: '/available-pools',
+    element: <PoolsPage poolsType={'All Pools'} />,
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [devModeState, setDevModeState] = useState(false);
-  const versionGitTag = import.meta.env.VITE_REACT_APP_GIT_TAG;
-  const gitDate = import.meta.env.VITE_REACT_APP_GIT_DATE;
-
-  console.log('VITE_REACT_APP_GIT_TAG - ', versionGitTag);
-  console.log('VITE_REACT_APP_GIT_DATE - ', gitDate);
-
-  const handleToggleDevMode = (value: boolean) => {
-    setDevModeState(value);
-  };
-
-  return (
-    <div className='app appBox'>
-      {versionGitTag && gitDate && (
-        <div>
-          <DevModeToggle isDevMode={devModeState} handleToggleDevMode={handleToggleDevMode} />
-          {devModeState && (
-            <div>
-              <p>Version: {versionGitTag}</p>
-              <p>Date: {gitDate}</p>
-            </div>
-          )}
-        </div>
-      )}
-      <div className='logoBox'>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>New</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-      <ConnectButton />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
