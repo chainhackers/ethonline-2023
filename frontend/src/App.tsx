82c98a5e21d3c40f101abf33ee9e7c6a02885696
nadyasav <nadyasav28@gmail.com>
2023-10-11 03:02:54 +0300

    changed button-toggle to text box for version and date info #8

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,34 +5,18 @@ import './index.scss';
 import './App.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import DevModeToggle from './components/devModeToggle/DevModeToggle';
+import VersionInfo from './components/versionInfo/VersionInfo';
 
 function App() {
   const [count, setCount] = useState(0);
-  const [devModeState, setDevModeState] = useState(false);
   const versionGitTag = import.meta.env.VITE_REACT_APP_GIT_TAG;
   const gitDate = import.meta.env.VITE_REACT_APP_GIT_DATE;
 
   console.log('VITE_REACT_APP_GIT_TAG - ', versionGitTag);
   console.log('VITE_REACT_APP_GIT_DATE - ', gitDate);
 
-  const handleToggleDevMode = (value: boolean) => {
-    setDevModeState(value);
-  };
-
   return (
     <div className='app appBox'>
-      {versionGitTag && gitDate && (
-        <div>
-          <DevModeToggle isDevMode={devModeState} handleToggleDevMode={handleToggleDevMode} />
-          {devModeState && (
-            <div>
-              <p>Version: {versionGitTag}</p>
-              <p>Date: {gitDate}</p>
-            </div>
-          )}
-        </div>
-      )}
       <div className='logoBox'>
         <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
@@ -50,6 +34,7 @@ function App() {
       </div>
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
       <ConnectButton />
+      {versionGitTag && gitDate && <VersionInfo version={versionGitTag} versionDate={gitDate} />}
     </div>
   );
 }
