c8c6c9b13db83c41ed6d84e205f20e227abccbd4
Joe Prime <101joe.prime@gmail.com>
2023-10-12 16:24:05 +0300

    add dateCell

---
+++ frontend/src/components/tableCells/dateCell/dateCell.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+import styles from './dateCell.module.scss';
+import { DateCellPropsI } from './dateCellProps';
+
+export const DateCell: FC<DateCellPropsI> = () => {
+  return <div>dateCell</div>;
+};
