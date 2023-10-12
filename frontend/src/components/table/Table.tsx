26d9f06827e54f985d038f23dccc1dc14aedc69b
Joe Prime <101joe.prime@gmail.com>
2023-10-12 16:24:39 +0300

    fix ts error

--- frontend/src/components/table/Table.tsx
+++ frontend/src/components/table/Table.tsx
@@ -1,9 +1,9 @@
 import { TablePropsI } from './TableProps.ts';
 import styles from './Table.module.scss';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
-export const Table: FC<TablePropsI> = ({}) => {
-  const pools = [
+export const Table: FC<TablePropsI> = () => {
+  const [pools, setPools] = useState([
     {
       address: '0x4681de1e080e889fad73853fa7adaa0be39e433f',
       date: 1696954119482,
@@ -46,7 +46,7 @@ export const Table: FC<TablePropsI> = ({}) => {
       profitPercent: 10,
       profit: 30000,
     },
-  ];
+  ]);
 
   const tableRowMap = {
     date: { title: 'Date', component: 'some tsx' },
@@ -54,22 +54,46 @@ export const Table: FC<TablePropsI> = ({}) => {
     total: { title: 'Total', component: 'some tsx' },
     profit: { title: 'Profit', component: 'some tsx' },
   };
+  //todo: combine tokens and anchorCurrency for Anchor currency / Tokens
+  // const combineTokens = (pools: any) => {
+  //   const result = pools.map((pool: any) => {
+  //     return (pool = {
+  //       ...pool,
+  //       allTokens: { anchorCurrency: pool.anchorCurrency, tokens: pool.tokens },
+  //     });
+  //   });
+  //   setPools(result);
+  // };
 
+  // useEffect(() => {
+  //   combineTokens(pools);
+  // }, [pools]);
+  //todo: find proper place for interfaces and types
+  type PoolT = {
+    address: string;
+    date: number;
+    anchorCurrency: string;
+    tokens: string[];
+    operatorFee: number;
+    total: number;
+    profitPercent: number;
+    profit: number;
+  };
   return (
     <div className={styles.tableContainer}>
       <table border={1}>
         <thead>
           <tr>
             {Object.entries(tableRowMap).map(([key, value]) => (
-              <th key={value.title}>{value.title}</th>
+              <th key={key}>{value.title}</th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {pools.map((pool: any) => (
+          {pools.map((pool) => (
             <tr key={pool.address}>
-              {Object.entries(tableRowMap).map(([key, value]) => (
-                <td key={key}>{pool[key]}</td>
+              {Object.keys(tableRowMap).map((key) => (
+                <td key={key}>{pool[key as keyof PoolT]}</td>
               ))}
             </tr>
           ))}
