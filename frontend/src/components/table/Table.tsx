e4722ba14ac9af871e9e6885c4303917ecaf39f4
Joe Prime <101joe.prime@gmail.com>
2023-10-12 20:01:24 +0300

    add cellDate to table

--- frontend/src/components/table/Table.tsx
+++ frontend/src/components/table/Table.tsx
@@ -1,12 +1,13 @@
 import { TablePropsI } from './TableProps.ts';
 import styles from './Table.module.scss';
 import { FC } from 'react';
+import { DateCell } from '../index.ts';
 
 export const Table: FC<TablePropsI> = () => {
   const pools = [
     {
       address: '0x4681de1e080e889fad73853fa7adaa0be39e433f',
-      date: 1696954119482,
+      date: 1697123492289,
       anchorCurrency: '0x6265617665726275696c642e6f7267',
       tokens: [
         '0x6265617665726275696c642e6f7267',
@@ -20,7 +21,7 @@ export const Table: FC<TablePropsI> = () => {
     },
     {
       address: '0x4681de1e080e889fad73853fa7adaa0be39e433z',
-      date: 1696954119482,
+      date: 1697123492289,
       anchorCurrency: '0x6265617665726275696c642e6f7267',
       tokens: [
         '0x6265617665726275696c642e6f7267',
@@ -34,7 +35,7 @@ export const Table: FC<TablePropsI> = () => {
     },
     {
       address: '0x4681de1e080e889fad73853fa7adaa0be39e433x',
-      date: 1696954119482,
+      date: 1697123492289,
       anchorCurrency: '0x6265617665726275696c642e6f7267',
       tokens: [
         '0x6265617665726275696c642e6f7267',
@@ -49,10 +50,10 @@ export const Table: FC<TablePropsI> = () => {
   ];
 
   const tableRowMap = {
-    date: { title: 'Date', component: 'some tsx' },
-    operatorFee: { title: 'Operator fee', component: 'some tsx' },
-    total: { title: 'Total', component: 'some tsx' },
-    profit: { title: 'Profit', component: 'some tsx' },
+    date: { title: 'Date', component: 'DateCell' },
+    // operatorFee: { title: 'Operator fee', component: 'some tsx' },
+    // total: { title: 'Total', component: 'DateCell' },
+    // profit: { title: 'Profit', component: 'some tsx' },
   };
   //todo: combine tokens and anchorCurrency for Anchor currency / Tokens
   // const combineTokens = (pools: any) => {
@@ -79,6 +80,8 @@ export const Table: FC<TablePropsI> = () => {
     profitPercent: number;
     profit: number;
   };
+  const cells = { DateCell };
+  type CellsT = typeof cells;
   return (
     <div className={styles.tableContainer}>
       <table border={1}>
@@ -92,9 +95,14 @@ export const Table: FC<TablePropsI> = () => {
         <tbody>
           {pools.map((pool) => (
             <tr key={pool.address}>
-              {Object.keys(tableRowMap).map((key) => (
-                <td key={key}>{pool[key as keyof PoolT]}</td>
-              ))}
+              {Object.entries(tableRowMap).map(([key, value]) => {
+                const TagName = cells[value.component as CellsT];
+                return (
+                  <td key={key}>
+                    <TagName cellValue={pool[key as keyof PoolT]} />
+                  </td>
+                );
+              })}
             </tr>
           ))}
         </tbody>
