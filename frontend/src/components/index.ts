27d8a205ac59d005c57654e9b780d8eb947c2e01
Joe Prime <101joe.prime@gmail.com>
2023-10-13 15:09:41 +0300

    fix import #9

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,4 +2,4 @@ export * from 'Components/table';
 export * from 'Components/tableCells';
 export * from 'Components/pagination';
 export * from 'Components/header';
-export * from 'Components/tableCells';
+export * from 'Components/tableCells/dateCell';
