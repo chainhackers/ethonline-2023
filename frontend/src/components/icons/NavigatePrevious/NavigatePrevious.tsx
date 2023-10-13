381fa66c2a8333938a32510bb75326c64b4bf42a
Joe Prime <101joe.prime@gmail.com>
2023-10-13 14:51:59 +0300

    add header #9

---
+++ frontend/src/components/icons/NavigatePrevious/NavigatePrevious.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+import styles from './NavigatePrevious.module.scss';
+
+export const NavigatePrevious: FC = () => {
+  return (
+    <svg
+      className={styles.icon}
+      xmlns='http://www.w3.org/2000/svg'
+      width='40'
+      height='40'
+      viewBox='0 0 40 40'
+      fill='none'
+    >
+      <path
+        d='M22.5898 14.0015L23.9998 15.4115L19.4198 20.0015L23.9998 24.5915L22.5898 26.0015L16.5898 20.0015L22.5898 14.0015Z'
+        fill='#43474E'
+      />
+    </svg>
+  );
+};
