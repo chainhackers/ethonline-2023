8662d5ae4c9fbc2f3be84afdaf46cf06599a8f2a
Joe Prime <101joe.prime@gmail.com>
2023-10-15 17:56:26 +0300

    add read contract

--- frontend/src/pages/PoolsPage/PoolsPage.tsx
+++ frontend/src/pages/PoolsPage/PoolsPage.tsx
@@ -2,8 +2,18 @@ import { PoolsPagePropsI } from './PoolsPageProps.ts';
 import styles from './PoolsPage.module.scss';
 import { FC } from 'react';
 import { Header, Table } from 'Components';
+import { erc20ABI, useAccount, useContractRead } from 'wagmi';
 
 export const PoolsPage: FC<PoolsPagePropsI> = ({ poolsType }) => {
+  const { address } = useAccount();
+  const { data } = useContractRead({
+    address: '0xCa9c4a7949e6f9dc8343b565E34C493E4970c1AB',
+    abi: erc20ABI,
+    functionName: 'balanceOf',
+    args: [address!],
+    watch: true,
+  });
+
   return (
     <>
       <Header />
