c6ce96a04d65686763315f740093daf8ccba8335
Joe Prime <101joe.prime@gmail.com>
2023-10-16 07:38:10 +0300

    remove base #9

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     assetsDir: 'assets',
   },
 
-  base: './',
+  // base: './',
   resolve: {
     alias: {
       Components: path.resolve(__dirname, './src/components'),
