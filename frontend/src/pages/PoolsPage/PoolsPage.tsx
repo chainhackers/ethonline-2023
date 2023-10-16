d00c55a75483c987233e7b879c21ef5a62fde8ee
Joe Prime <101joe.prime@gmail.com>
2023-10-16 07:39:33 +0300

    get contract name #9

--- frontend/src/pages/PoolsPage/PoolsPage.tsx
+++ frontend/src/pages/PoolsPage/PoolsPage.tsx
@@ -1,18 +1,50 @@
 import { PoolsPagePropsI } from './PoolsPageProps.ts';
 import styles from './PoolsPage.module.scss';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Header, Table } from 'Components';
-import { erc20ABI, useAccount, useContractRead } from 'wagmi';
+import { useContractRead } from 'wagmi';
+import { abi } from '../../data/abi.ts';
+// import { publicClientViem } from 'src/wagmiConfig.ts';
+const contract = '0xCa9c4a7949e6f9dc8343b565E34C493E4970c1AB';
 
 export const PoolsPage: FC<PoolsPagePropsI> = ({ poolsType }) => {
-  const { address } = useAccount();
-  const { data } = useContractRead({
-    address: '0xCa9c4a7949e6f9dc8343b565E34C493E4970c1AB',
-    abi: erc20ABI,
-    functionName: 'balanceOf',
-    args: [address!],
-    watch: true,
+  const { data, isSuccess } = useContractRead({
+    address: contract,
+    abi: abi,
+    functionName: 'name',
+    watch: false,
   });
+  useEffect(() => {
+    if (isSuccess) {
+      console.log(data);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSuccess]);
+
+  // const wagmiContract = {
+  //   address: contract,
+  //   abi: abi,
+  //   functionName: 'name',
+  // } as const;
+
+  // const getPools = async () => {
+  //   const results = await publicClientViem.multicall({
+  //     contracts: [
+  //       {
+  //         ...wagmiContract,
+  //       },
+  // {
+  //   ...wagmiContract,
+  //   functionName: 'ownerOf',
+  //   args: [69420n],
+  // },
+  // {
+  //   ...wagmiContract,
+  //   functionName: 'mint',
+  // },
+  //     ],
+  //   });
+  // };
 
   return (
     <>
