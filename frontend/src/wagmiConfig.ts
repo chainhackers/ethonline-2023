81e2318ddb5f664114cc09bdbcff9987bbd559ed
Joe Prime <101joe.prime@gmail.com>
2023-10-16 07:37:56 +0300

    add publicClientViem #9

--- frontend/src/wagmiConfig.ts
+++ frontend/src/wagmiConfig.ts
@@ -3,10 +3,15 @@ import { configureChains, createConfig } from 'wagmi';
 import { polygon } from '@wagmi/core/chains';
 import { getDefaultWallets } from '@rainbow-me/rainbowkit';
 import { PROJECT_ID } from './constants/constants';
+import { createPublicClient, http } from 'viem';
 
 console.log('walletConnect PROJECT_ID: ', PROJECT_ID);
 
 export const { chains, publicClient } = configureChains([polygon], [publicProvider()]);
+export const publicClientViem = createPublicClient({
+  chain: polygon,
+  transport: http(),
+});
 
 const { connectors } = getDefaultWallets({
   appName: 'profitpals',
